2708bd5053e6e68334e9ec0143f121f762498454
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2009-06-13 14:52:53 +0300

    * Implemented moving view

--- Fractal.hs
+++ Fractal.hs
@@ -1,6 +1,8 @@
 module Fractal where
 
-import Data.Word
+import Data.Word (Word8)
+
+type Area = (Double, Double, Double, Double) -- x0, y0, x1, y1
 
 getIterColor :: Integer -> Integer -> (Word8, Word8, Word8)
 getIterColor iter max | iter == 1 = (255, 255, 255)
@@ -8,10 +10,10 @@ getIterColor iter max | iter == 1 = (255, 255, 255)
                       | otherwise = (c, c, c)
                       where c = fromInteger $ (iter * 11) `mod` 256
 
-pixelToCoord :: Integer -> Integer -> Integer -> Integer -> (Double, Double)
-pixelToCoord w h i j = (-2.0 + (fromInteger i * dx + dx / 2.0), 1 - (fromInteger j * dy + dy / 2.0))
-    where dx = 3.0 / fromInteger w
-          dy = 2.0 / fromInteger h
+pixelToCoord :: Integer -> Integer -> Area -> Integer -> Integer -> (Double, Double)
+pixelToCoord w h (x0, y0, x1, y1) i j = (x0 + (fromInteger i * dx + dx / 2.0), y0 - (fromInteger j * dy + dy / 2.0))
+    where dx = abs (x1 - x0) / fromInteger w
+          dy = abs (y1 - y0) / fromInteger h
 
 getIter :: Integer -> Double -> Double -> Integer
 getIter max x y = getIter' max 0 x y x y
--- Main.hs
+++ Main.hs
@@ -1,10 +1,10 @@
 module Main where
 
-import Data.Char
-import Data.Word
-import Control.Monad
-import Foreign.Ptr
-import Foreign.Storable
+import Data.Char (ord, chr)
+import Data.Word (Word8)
+import Control.Monad (forM_)
+import Foreign.Ptr (plusPtr)
+import Foreign.Storable (poke)
 import Graphics.UI.SDL as SDL
 import Fractal
 
@@ -20,7 +20,7 @@ fractalStringRow w h max j = [fractalChar w h max i j | i <- [0..w]]
 
 fractalChar :: Integer -> Integer -> Integer -> Integer -> Integer -> Char
 fractalChar w h max i j = c
-    where (x, y) = pixelToCoord w h i j
+    where (x, y) = pixelToCoord w h (-2.0, 1.0, 1.0, -1.0) i j
           iter = getIter max x y
           c = iterToChar (fromIntegral iter)
 
@@ -28,12 +28,12 @@ iterToChar :: Int -> Char
 iterToChar n = chr (ord '0' + n `mod` 10)
 ---
 
-drawFractal :: Surface -> Integer -> Integer -> Integer -> IO ()
-drawFractal s w h max = forM_ [(i, j) | j <- [0..(h - 1)], i <- [0..(w - 1)]] (drawPixel s w h max)
+drawFractal :: Surface -> Integer -> Integer -> Area -> Integer -> IO ()
+drawFractal s w h area max = forM_ [(i, j) | j <- [0..(h - 1)], i <- [0..(w - 1)]] (drawPixel s w h area max)
 
-drawPixel :: Surface -> Integer -> Integer -> Integer -> (Integer, Integer) -> IO ()
-drawPixel s w h max (i, j) = setPixel s i j c
-    where c = getPixelColor w h i j max
+drawPixel :: Surface -> Integer -> Integer -> Area -> Integer -> (Integer, Integer) -> IO ()
+drawPixel s w h area max (i, j) = setPixel s i j c
+    where c = getPixelColor w h area i j max
 
 --Color 255 (fromIntegral (i `mod` 255)) (fromIntegral (j `mod` 255))
 --getPixelColor w h i j max
@@ -58,28 +58,34 @@ setPixel s i j (Color r g b) = do
     3 -> error "TODO: bpp 3" -- TODO
     4 -> poke pixelPtr pixel
 
-getPixelColor :: Integer -> Integer -> Integer -> Integer -> Integer -> Color
-getPixelColor w h i j max = colorWrapper $ getIterColor (getIter max x y) max
-    where (x, y) = pixelToCoord w h i j
+getPixelColor :: Integer -> Integer -> Area -> Integer -> Integer -> Integer -> Color
+getPixelColor w h area i j max = colorWrapper $ getIterColor (getIter max x y) max
+    where (x, y) = pixelToCoord w h area i j
 
 colorWrapper :: (Word8, Word8, Word8) -> Color
 colorWrapper (r, g, b) = Color r g b
 
-
-width = 600
-height = 400
+width = 300
+height = 200
 maxIter = 10
+startArea = (-2, 1, 1, -1)
 
-
+main :: IO ()
 main = do
   SDL.init [InitEverything]
   setVideoMode width height 32 []
   screen <- getVideoSurface
+  drawScreen screen startArea
+  eventHandler screen startArea
+
+
+drawScreen :: Surface -> Area -> IO ()
+drawScreen screen area = do
   lockSurface screen
-  drawFractal screen (toInteger width) (toInteger height) maxIter
+  drawFractal screen (toInteger width) (toInteger height) area maxIter
   unlockSurface screen
   updateRect screen (Rect 0 0 width height)
-  eventHandler
+
 
 {-
 main = do
@@ -87,9 +93,37 @@ main = do
 -}
 
 
-eventHandler :: IO ()
-eventHandler = do
+eventHandler :: Surface -> Area -> IO ()
+eventHandler screen area = do
   e <- waitEvent
   case e of
     Quit -> return ()
-    otherwise -> eventHandler
+    KeyDown keysym -> keyHandler screen area keysym
+    otherwise -> eventHandler screen area
+
+keyHandler :: Surface -> Area -> Keysym -> IO ()
+keyHandler screen area@(x0, y0, x1, y1) keysym
+    | symKey keysym == SDLK_ESCAPE = return ()
+    | symKey keysym == SDLK_RETURN = do 
+  drawScreen screen area
+  eventHandler screen area
+    | symKey keysym == SDLK_UP = do
+  drawScreen screen areaUp
+  eventHandler screen areaUp
+    | symKey keysym == SDLK_DOWN = do
+  drawScreen screen areaDown
+  eventHandler screen areaDown
+    | symKey keysym == SDLK_LEFT = do
+  drawScreen screen areaLeft
+  eventHandler screen areaLeft
+    | symKey keysym == SDLK_RIGHT = do
+  drawScreen screen areaRight
+  eventHandler screen areaRight
+    | otherwise = eventHandler screen area
+    where dx = abs (x1 - x0) / 10
+          dy = abs (y1 - y0) / 10
+          areaUp = (x0, y0 + dy, x1, y1 + dy)
+          areaDown = (x0, y0 - dy, x1, y1 - dy)
+          areaLeft = (x0 - dx, y0, x1 - dx, y1)
+          areaRight = (x0 + dx, y0, x1 + dx, y1)
+
